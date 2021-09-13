3f11f2d864d0fc97657100352799309192f65a50
Jeff Jessie <j3ffjessie@protonmail.com>
2021-09-13 12:35:41 -0500

    Adjust celebrations setup by creating an object template and then creating a new User off of that template. Will allow for easy creation of a new user story for this section.

--- assets/js/celebrations.js
+++ assets/js/celebrations.js
@@ -1,14 +1,19 @@
 
+//creating our User object that will be used to setup the rest of the objects that we utilize for this template string
 
-const user = {
-    id: '1',
-    fName: 'Joe',
-    lName: 'Smith',
-    celebration: '"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"',
-    image: '/assets/images/team-members/likii.jpg'
+function User(fName, lName, celebration, image) {
+    this.fName= fName;
+    this.lName= lName;
+    this.celebration= celebration;
+    this.image= image;
 }
 
 
+//Creating our first New User based on our object template and passing in the values
+let user1 = new User("Joe", "Smith", '"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"', '/assets/images/team-members/likii.jpg');
+
+
+//Creating our HTML template markup to take the above object information and populate it into our HTML file in a structured way that makes it cleaner inside the HTML file
 const markup = `
 
 <div class="card">
@@ -16,16 +21,19 @@ const markup = `
     “
     </div>
     <div class="avatar">
-    <img class="avatar-image" src= ${user.image} />
+    <img class="avatar-image" src= ${user1.image} />
 </div>
     <div class="card-content">
-    ${user.celebration}
+    ${user1.celebration}
     </div>
-    <p class='card-text'> - ${user.fName} ${user.lName}</p>
+    <p class='card-text'> - ${user1.fName} ${user1.lName}</p>
 
 </div>
 
 `;
+//Setting our markup to replace the innerHTML of our <div> element with the ID of celebrate
 
 document.getElementById('celebrate').innerHTML= markup;
 
+
+// ToDo -Figure out how to randomize the picking of a user object to display in the html template string markup
