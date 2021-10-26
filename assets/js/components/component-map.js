a29473db45decc0fd335d86598308b65e97213d9
Matea Howard <bl0wfacee@gmail.com>
2021-10-26 20:11:14 +0100

    adding analytics

--- assets/js/components/component-map.js
+++ assets/js/components/component-map.js
@@ -1,5 +1,7 @@
 import { Sidebar } from '../components/sidebar/sidebar.js';
 import { Footer } from '../components/footer/footer.js';
+import { GDPR } from '../components/gdpr/gdpr.js';
 
 customElements.define('sidebar-component', Sidebar);
 customElements.define('footer-component', Footer);
+customElements.define('gdpr-component', GDPR);
