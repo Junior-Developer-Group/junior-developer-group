c99db3a99ed9072ed54cfb213cc5c0b13d5645f6
A-amon <64846892+A-amon@users.noreply.github.com>
2021-11-27 11:00:20 +0800

    Add Repositories sidebar link Add Repositories page meta tags

--- assets/js/components/sidebar/sidebar.js
+++ assets/js/components/sidebar/sidebar.js
@@ -10,5 +10,38 @@ export class Sidebar extends HTMLElement {
       this,
       '../assets/js/components/sidebar/sidebar.html'
     );
+
+	window.onload = () => {	// Run after DOM is loaded
+		this.setActiveLink();
+	}
+  }
+
+  /**
+   * Switch .active link on sidebar
+   * Based on current page URL
+   */
+  setActiveLink(){
+	  /**
+	   * Unset previous .active link
+	   */
+	const currentActive = document.querySelector('.active');
+	if(currentActive){
+		currentActive.classList.remove('active');
+		currentActive.removeAttribute('aria-current');
+	}
+
+	/**
+	 * Set new .active link
+	 */
+	const currentPage = window.location.href;
+	const navLinks = document.querySelectorAll('.nav-link');
+	for(const navLink of navLinks){
+		const linkHref = navLink.getAttribute('href');
+		if(currentPage.includes(linkHref)){
+			navLink.classList.add('active');
+			navLink.setAttribute('aria-current','page');
+			break;
+		}
+	}
   }
 }
