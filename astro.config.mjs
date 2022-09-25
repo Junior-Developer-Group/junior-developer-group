cd766eb29e9ad6b48ee5f8d14daf53ff37bef6f5
Matea Howard <bl0wfacee@gmail.com>
2022-09-25 19:04:40 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://junior-developer-group.com/',
+  base: '/junior-developer-group',
   integrations: [vue(), sitemap()]
 });
